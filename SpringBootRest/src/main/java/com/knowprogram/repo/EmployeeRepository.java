2f898f2cdb464f2ea4103fafa4a4ad7b14dc3436
Know Program <83252909+Know-Program@users.noreply.github.com>
2022-07-11 14:59:01 +0530

    Delete SpringBootRest directory

--- SpringBootRest/src/main/java/com/knowprogram/repo/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.knowprogram.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.knowprogram.model.Employee;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
-
-}
